8bff90b89230d4445dd7749606da1e75b2ae5383
Thai Tran-Duy <thaitd@pc-187.atalink.com>
2021-03-26 16:57:04 +0700

    Upate code

--- src/app/components/ReactWindow/index.js
+++ src/app/components/ReactWindow/index.js
@@ -1,19 +1,64 @@
-import faker, { Company, Address } from "Faker";
+import faker, {
+  Company,
+  Address,
+  Internet,
+  PhoneNumber,
+  Lorem,
+  Name,
+  Image,
+} from "Faker";
 import Virtualize from "../Virtualize";
 faker.locale = "en";
 
 const ReactWindow = () => {
-  const number = 1000000;
+  const number = 10000;
   const array = new Array(number).fill();
+
   const generatedList = () =>
     array.map(() => ({
       name: Company.companyName(),
+      bussiness: Company.bs(),
+      founder: Name.findName(),
+      manager: Name.findName(),
       street: Address.streetAddress(),
       city: Address.city(),
       country: Address.ukCountry(),
+      zipCode: Address.zipCode(),
+      email: Internet.email(),
+      phone: PhoneNumber.phoneNumber(),
+      secondPhone: PhoneNumber.phoneNumber(),
+      website: Internet.domainName(),
+      slogen: Lorem.words(),
+      description: Lorem.sentence(),
+      image: Image.avatar(),
     }));
 
-  return <Virtualize data={generatedList()} viewHeight={500} itemHeight={50} />;
+  const columns = [
+    "Company Name",
+    "Bussiness",
+    "Founder",
+    "Manager",
+    "Street",
+    "City",
+    "Country",
+    "Zip Code",
+    "Email",
+    "Phone",
+    "Second Phone",
+    "Website",
+    "Slogen",
+    "Description",
+    "Avatar",
+  ];
+
+  return (
+    <Virtualize
+      data={generatedList()}
+      columns={columns}
+      viewHeight={500}
+      itemHeight={50}
+    />
+  );
 };
 
 export default ReactWindow;
--- src/app/components/Virtualize/index.js
+++ src/app/components/Virtualize/index.js
@@ -2,14 +2,43 @@ import { useState, useEffect } from "react";
 import PerfectScrollbar from "react-perfect-scrollbar";
 import styled from "styled-components";
 
-const StyledScroll = styled(PerfectScrollbar)`
+const StyledScroll = styled(PerfectScrollbar)``;
 
+const Row = styled.div`
+  display: grid;
+  grid-template-columns: ${(props) => `repeat(${props.columnsNumber}, 600px)`};
+  height: ${(props) => `${props.height}px`};
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${(props) =>
+      props.position % 2 === 0 ? "#f1f1f1" : "inherit"};
+  }
 `;
 
-const Virtualize = ({ data, viewHeight, itemHeight }) => {
+const Header = styled.div`
+  display: grid;
+  grid-template-columns: ${(props) => `repeat(${props.columnsNumber}, 600px)`};
+  height: 50px;
+  position: sticky;
+  top: 0;
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 600;
+    border-bottom: 1px solid #f1f1f1;
+    box-shadow: 0 5px 20px -10px #e8e8e8;
+  }
+`;
+
+const Virtualize = ({ data, viewHeight, itemHeight, columns }) => {
   const totalItemOnView = Math.floor(viewHeight / itemHeight);
   const [renderList, setRenderList] = useState([]);
   const [top, setTop] = useState(0);
+  const [startPosition, setStartPosition] = useState(0);
 
   useEffect(() => {
     const initList = data.filter((item, index) => index < totalItemOnView);
@@ -19,28 +48,52 @@ const Virtualize = ({ data, viewHeight, itemHeight }) => {
   const calculating = (scrollTop) => {
     const addItem = Math.floor(scrollTop / itemHeight);
     const newList = data.filter(
-      (item, index) =>
-        index < totalItemOnView + addItem && index > addItem - 1
+      (item, index) => index < totalItemOnView + addItem && index > addItem - 1
     );
     setRenderList(newList);
     setTop(scrollTop);
+    setStartPosition(addItem);
   };
 
+  const renderValues = (item) =>
+    Object.values(item).map((value, index) => (
+      <div key={`col-${index}`}>{value}</div>
+    ));
+
   return (
-      <StyledScroll
-        style={{ height: `${viewHeight}px` }}
-        onScrollY={(container) => {
-          calculating(container.scrollTop);
+    <StyledScroll
+      style={{ height: `${viewHeight}px`, width: "100vw" }}
+      onScrollY={(container) => {
+        calculating(container.scrollTop);
+      }}
+    >
+      <div
+        style={{
+          height: `${data.length * itemHeight}px`,
+          position: "relative",
         }}
       >
-      <div style={{ height: `${data.length * itemHeight}px`, position: 'relative' }}>
-        <div style={{ position: 'absolute', top: `${top}px` }}>
-        {renderList.map((item, index) => (
-            <div key={index} style={{height: `${itemHeight}px`}}>{ item.name}</div>
+        <Header columnsNumber={columns.length}>
+          {columns.map((item, index) => (
+            <div key={`header-col-${index}`}>{item}</div>
+          ))}
+        </Header>
+        <div
+          style={{ position: "absolute", top: `${top + 50}px`, width: "100%" }}
+        >
+          {renderList.map((item, index) => (
+            <Row
+              key={`row-${index}`}
+              position={startPosition + index}
+              height={itemHeight}
+              columnsNumber={columns.length}
+            >
+              {renderValues(item)}
+            </Row>
           ))}
         </div>
-        </div>
-      </StyledScroll>
+      </div>
+    </StyledScroll>
   );
 };
 
